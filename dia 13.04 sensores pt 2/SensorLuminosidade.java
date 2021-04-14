8b8fb883fb3ef1a5a7a44799818c108b287417dd
RibasGuilherme <79951811+RibasGuilherme@users.noreply.github.com>
2021-04-13 22:17:39 -0300

    Add files via upload

---
+++ dia 13.04 sensores pt 2/SensorLuminosidade.java
@@ -0,0 +1,90 @@
+package com.example.sensoresn;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SensorLuminosidade extends AppCompatActivity {
+    private TextView visual;
+    SensorManager sm;
+    SensorEventListener listener;
+    Sensor luz;
+    Button btnAce;
+    Button btnInicio;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor_luminosidade);
+
+        visual = findViewById(R.id.visual1);
+        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        luz = sm.getDefaultSensor(Sensor.TYPE_LIGHT);
+        btnAce = findViewById(R.id.btnInicio);
+        btnInicio = findViewById(R.id.btnInicio);
+
+        btnInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirInicio();
+            }
+        });
+
+
+        btnAce.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abriAcele();
+            }
+        });
+
+        listener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event)
+            {
+                visual.setText(String.valueOf(event.values[0]));
+                int grayShade = (int) event.values[0];
+
+                if(grayShade > 255) grayShade = 255;
+
+                visual.setTextColor(Color.rgb( 255 - grayShade, 255 - grayShade, 255 - grayShade ) );
+                visual.setBackgroundColor(Color.rgb(grayShade,grayShade,grayShade));
+
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i)
+            {
+
+            }
+        };
+
+        sm.registerListener(listener,luz,SensorManager.SENSOR_DELAY_FASTEST);
+
+    }
+
+    public void abriAcele(){
+        Intent janela = new Intent(this, SensorManager.class);
+        startActivity(janela);
+    }
+
+    @Override
+    protected void onPause() {
+
+        sm.unregisterListener(listener,luz);
+        super.onPause();
+    }
+
+    public void abrirInicio () {
+        Intent janela = new Intent(this, MainActivity.class);
+        startActivity(janela);
+    }
+}
---
+++ dia 13.04 sensores pt 2/SensorProximidade.java
@@ -0,0 +1,116 @@
+package com.example.sensoresn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+//declaração da classe implements com a abstração do SensorEventListener
+
+public class SensorProximidade extends AppCompatActivity implements SensorEventListener {
+    //Declaração de atribultos
+    private Sensor proximidade;
+    private SensorManager medir;
+    private TextView resposta;
+    private Button btnVoltar, btnLuminosidade;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor_proximidade);
+
+        //Mapeamento de todos os objetos do layout
+
+        medir(SensorManager) = this.getSystemService(SENSOR_SERVICE);
+        proximidade = medir.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        resposta = findViewById(R.id.resposta);
+        btnVoltar = findViewById(R.id.btnVoltar);
+        btnLuminosidade = findViewById(R.id.btnLuminosidade);
+
+
+        //Eventos de botões
+
+        btnLuminosidade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AbrirLuminosidade();
+            }
+        });
+
+
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AbrirVoltar();
+            }
+        });
+
+    }
+
+    //chamandas de callbakcs
+
+    @Override
+    protected void onPause() {
+        medir.registerListener(this, proximidade, SensorManager.SENSOR_DELAY_NORMAL);
+        super.onPause();
+    }
+
+    //método ocorre quando e execultado ou modificado algum tipo de evento relacionado ao sensor
+    //especifico
+    //se o valor do evento for = 0, é pq estamos próximos ao sensor, senao estamos afastados
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.values [0] ==0)
+        {
+            getWindow().getDecorView().setBackgroundColor(color.LIGHTBLUE);
+            resposta.setText("Está Próximo.");
+        }else
+        {
+            getWindow().getDecorView().setBackgroundColor(color.RED);
+            resposta.setText("Está Afastado.");
+        }
+    }
+
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        medir.registerListener(this, proximidade, SensorManager.SENSOR_DELAY_NORMAL);
+
+        super.onResume();
+    }
+
+    //abertura de activitys
+    public void AbrirLuminosidade(){
+        Intent janela = new Intent(this,SensorLuminosidade.class);
+        startActivity(janela);
+    }
+    public void AbrirVoltar(){
+        Intent janelav = new Intent(this,MainActivityS.class);
+        startActivity(janelav);
+    }
+
+}
+
